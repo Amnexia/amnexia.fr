7440cca9bea41369b716a216e391f66b37877e2e
Amnexia <contact@amnexia.fr>
2018-02-04 07:19:49 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,1 +1,13 @@
+// La fonction ' mail ' permet de créer un mail.
+mail($mail_destinataire, "Sujet du mail", $message, $header);
 
+// Pour récuperer un $mail tu effectue une requête sur ta base de donnée ou avec un $_POST['mail']
+// Pour la requête :
+
+$req_user = $bdd->prepare('SELECT * FROM membres WHERE id = ?');
+$req_user->execute(array($_SESSION['id']));
+$user = $req_user->fetch();
+
+
+$mail = $user['mail'];
+$mail_destinataire = $mail;
